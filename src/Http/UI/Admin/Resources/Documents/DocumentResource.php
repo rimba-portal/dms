5c5eed22abefc9e53e4b7b8e0af75793196cca08
Faros <114036707+a-faros@users.noreply.github.com>
2026-08-11 10:17:04 +0800

    Release v1.0.14

--- src/Http/UI/Admin/Resources/Documents/DocumentResource.php
+++ src/Http/UI/Admin/Resources/Documents/DocumentResource.php
@@ -22,7 +22,7 @@ class DocumentResource extends Resource
 {
     protected static ?string $model = Document::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::ArrowSmallRight;
 
     protected static ?string $recordTitleAttribute = 'title';
 
--- src/Http/UI/Staff/Resources/Documents/DocumentResource.php
+++ src/Http/UI/Staff/Resources/Documents/DocumentResource.php
@@ -20,7 +20,7 @@ class DocumentResource extends Resource
 {
     protected static ?string $model = Document::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::ArrowSmallRight;
 
     protected static string|UnitEnum|null $navigationGroup = 'Knowledge';
 
